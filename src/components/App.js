f2ed9e2ec9d1cc13b7f35d714cbbfd6caeb52703
shliahtur <shliahtur@gmail.com>
2021-02-12 12:37:35 +0200

    redux app structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.scss';
+
+const App = () => {
+    return (
+        <div className="wrapper">
+            <input type='text' />
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default App;
